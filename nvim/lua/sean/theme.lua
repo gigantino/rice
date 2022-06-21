c8336c00c571e05d6c6de8f0e171e5d9559dbc92
gigantino <29411889+gigantino@users.noreply.github.com>
2022-06-21 14:03:34 +0200

    [nvim] updated lsp, themes and options

--- nvim/lua/sean/theme.lua
+++ nvim/lua/sean/theme.lua
@@ -1,8 +1,15 @@
+-- Catppuccin settings
 vim.g.catppuccin_flavour = "mocha"
-local colorscheme = "catppuccin"
+-- Onedark settings
+require("onedark").setup {
+	style = "deep"
+}
+-- Selected color scheme
+local colorscheme = "onedark"
 
+-- Notifies the user if the color scheme can't be found
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-	vim.notify("colorscheme " .. colorscheme .. " not found!")
+	vim.notify("The colorscheme " .. colorscheme .. " was not found!")
 	return
 end
